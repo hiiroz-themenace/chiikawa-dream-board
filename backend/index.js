fb994721a9d7a96949b31703e08822c3ff22c6eb
hiiroz-themenace <lalawhuill@gamil.com>
2026-01-18 14:20:59 +0800

    fixed refresh issue

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,8 @@ const NOTES_FILE = path.join(__dirname, "notes.json");
 app.use(cors());
 app.use(express.json());
 
+app.use(express.static(path.join(__dirname, '../frontend')));
+
 const storage = {
     read() {
         try {
